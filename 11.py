eb742bb573faa9fc34648834a22f6735df104a76
Oliver Daniel <oliverdaniel00@gmail.com>
2024-12-11 10:01:43 -0500

    thaaaaaaaat's better

--- 11.py
+++ 11.py
@@ -1,76 +1,48 @@
 """Solutions for Day 11."""
 
-from typing import Union
+from collections import defaultdict
 
 
 N = open('./in/11.txt').readline()
 # N = open('./in/11.test.1.txt').readline()
 
 DEBUG = 1
-data = N.strip().split()
+data = defaultdict(int)
+
+for stone in map(int, N.split()):
+    data[stone] += 1
 
 
-class Node(object):
-    value: Union[int,  None]
-    left: Union['Node',  None]
-    right: Union['Node',  None]
+def blink(stns):
+    new = defaultdict(int)
+    for k, v in stns.items():
+        if len(strval := str(k)) % 2 == 0:
+            L = len(strval)
+            new_left = int(strval[:L // 2])
+            new_right = int(strval[L // 2:])
 
-    def __init__(self, value: int):
-        self.value = int(value)
-        self.left = None
-        self.right = None
+            new[new_left] += v
+            new[new_right] += v
+        else:
+            new[k * 2024 or 1] += v
 
-    def __repr__(self):
-        if self.value is not None:
-            return f'<{self.value}>'
-        return f'<[{self.left}, {self.right}]>'
+    return new
 
 
 def p1(data):
-    def blink(node: Node):
-        if node is None:
-            return
-
-        if node.value is None:
-            blink(node.left)
-            blink(node.right)
-            return
-
-        if node.value == 0:
-            node.value = 1
-            return
-
-        if len(strval := str(node.value)) % 2 == 0:
-            new_left = Node(strval[:len(strval) // 2])
-            new_right = Node(strval[len(strval) // 2:])
-            if new_left.value is None:
-                print ("uh oh!", strval)
-            node.left = new_left
-            node.right = new_right
-            node.value = None
-            return
-
-        node.value *= 2024
-
-    curr = [Node(int(v)) for v in data]
-    # print(curr)
+    curr = data
     for _ in range(25):
-        for node in curr:
-            blink(node)
-        # print(curr)
+        curr = blink(curr)
 
-    def weight(node: Node):
-        if node is None:
-            return 0
-        if node.value is not None:
-            return 1
-        return weight(node.left) + weight(node.right)
-    
-    return sum(map(weight, curr))
+    return sum(curr.values())
 
 
 def p2(data):
-    pass
+    curr = data
+    for _ in range(75):
+        curr = blink(curr)
+
+    return sum(curr.values())
 
 
 if __name__ == "__main__":
